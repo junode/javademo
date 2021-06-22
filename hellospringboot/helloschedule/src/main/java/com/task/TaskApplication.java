3ce1a62392e58d517b1c1157a19d0b74a4e20798
zhunode <zhunode@163.com>
2021-06-23 07:54:06 +0800

    add note

--- hellospringboot/helloschedule/src/main/java/com/task/TaskApplication.java
+++ hellospringboot/helloschedule/src/main/java/com/task/TaskApplication.java
@@ -1,8 +1,5 @@
 package com.task;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
