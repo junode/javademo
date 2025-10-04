061493518643b3c89f819b94338a6e0a8b129628
junode <33363493+junode@users.noreply.github.com>
2025-10-04 09:16:52 +0800

    Delete sc-demo directory

--- sc-demo/sk-common/src/main/java/spring/cloud/learn/annotation/inherited/ClassC.java
+++
@@ -1,9 +0,0 @@
-package spring.cloud.learn.annotation.inherited;
-
-/**
- * @Description TODO
- * @Author junode
- * @Date 2020/12/23
- */
-public class ClassC extends ClassB{
-}
--- sc-demo/sk-common/src/main/java/spring/cloud/learn/annotation/inherited/ClassD.java
+++
@@ -1,9 +0,0 @@
-package spring.cloud.learn.annotation.inherited;
-
-/**
- * @description: 继承ClassA类
- * @author: junode
- * @create: 2020-12-23 22:20
- **/
-public class ClassD extends ClassA{
-}
